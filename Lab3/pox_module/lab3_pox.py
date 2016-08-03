32ff015d608fa28c805b95ed5f4de5ffd0828959
Wei Chen <weichen@email.arizona.edu>
2016-08-02 21:54:13 -0700

    added firewall code

--- Lab3/pox_module/lab3_pox.py
+++ Lab3/pox_module/lab3_pox.py
@@ -1,4 +1,4 @@
-# Copyright 2011-2012 James McCauley
+# Copyright 2012-2013 James McCauley
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,148 +13,324 @@
 # limitations under the License.
 
 """
-A simple router for Lab 3. 
+A stupid L3 switch
 
-It is derived from the L2 learning switch in POX.
-Routing functions are added so students can just edit the rtable file.
+For each switch:
+1) Keep a table that maps IP addresses to MAC addresses and switch ports.
+   Stock this table using information from ARP and IP packets.
+2) When you see an ARP query, try to answer it using information in the table
+   from step 1.  If the info in the table is old, just flood the query.
+3) Flood all other ARPs.
+4) When you see an IP packet, if you know the destination port (because it's
+   in the table from step 1), install a flow for it.
 """
 
 from pox.core import core
-import pox.openflow.libopenflow_01 as of
-from pox.lib.util import dpid_to_str
-from pox.lib.util import str_to_bool
-import time
-
+import pox
 log = core.getLogger()
 
-# We don't want to flood immediately when a switch connects.
-# Can be overriden on commandline.
-_flood_delay = 0
+from pox.lib.packet.ethernet import ethernet, ETHER_BROADCAST
+from pox.lib.packet.ipv4 import ipv4
+from pox.lib.packet.arp import arp
+from pox.lib.addresses import IPAddr, EthAddr
+from pox.lib.util import str_to_bool, dpid_to_str
+from pox.lib.recoco import Timer
 
-class LearningSwitch (object):
-  def __init__ (self, connection, transparent):
-    # Switch we'll be adding L2 learning switch capabilities to
-    self.connection = connection
-    self.transparent = transparent
+import pox.openflow.libopenflow_01 as of
 
-    # Our table
-    self.macToPort = {}
+from pox.lib.revent import *
 
-    # We want to hear PacketIn messages, so we listen
-    # to the connection
-    connection.addListeners(self)
+import time
 
-    # We just use this to know when to log a helpful message
-    self.hold_down_expired = _flood_delay == 0
+# Timeout for flows
+FLOW_IDLE_TIMEOUT = 10
 
-    #log.debug("Initializing LearningSwitch, transparent=%s",
-    #          str(self.transparent))
+# Timeout for ARP entries
+ARP_TIMEOUT = 60 * 2
+
+# Maximum number of packet to buffer on a switch for an unknown IP
+MAX_BUFFERED_PER_IP = 5
+
+# Maximum time to hang on to a buffer for an unknown IP in seconds
+MAX_BUFFER_TIME = 5
+
+# Our class for ARP entry
+class Entry (object):
+  """
+  Not strictly an ARP entry.
+  We use the port to determine which port to forward traffic out of.
+  We use the MAC to answer ARP replies.
+  We use the timeout so that if an entry is older than ARP_TIMEOUT, we
+   flood the ARP request rather than try to answer it ourselves.
+  """
+  def __init__ (self, port, mac):
+    self.timeout = time.time() + ARP_TIMEOUT
+    self.port = port
+    self.mac = mac
+
+  def __eq__ (self, other):
+    if type(other) == tuple:
+      return (self.port,self.mac)==other
+    else:
+      return (self.port,self.mac)==(other.port,other.mac)
+  def __ne__ (self, other):
+    return not self.__eq__(other)
+
+  def isExpired (self):
+    if self.port == of.OFPP_NONE: return False
+    return time.time() > self.timeout
+
+# fetch the mac address from the dpid: datapath id, which is used to identify switches in openflow
+def dpid_to_mac (dpid):
+  return EthAddr("%012x" % (dpid & 0xffFFffFFffFF,))
+
+
+class lab3_switch (EventMixin):
+  def __init__ (self, fakeways = [], arp_for_unknowns = False):
+    # These are "fake gateways" -- we'll answer ARPs for them with MAC
+    # of the switch they're connected to.
+    self.fakeways = set(fakeways)
+
+    # If this is true and we see a packet for an unknown
+    # host, we'll ARP for it.
+    self.arp_for_unknowns = arp_for_unknowns
+
+    # (dpid,IP) -> expire_time
+    # We use this to keep from spamming ARPs
+    self.outstanding_arps = {}
+
+    # (dpid,IP) -> [(expire_time,buffer_id,in_port), ...]
+    # These are buffers we've gotten at this datapath for this IP which
+    # we can't deliver because we don't know where they go.
+    self.lost_buffers = {}
+
+    # For each switch, we map IP addresses to Entries
+    self.arpTable = {}
+
+    # This timer handles expiring stuff
+    self._expire_timer = Timer(5, self._handle_expiration, recurring=True)
+
+    self.listenTo(core)
+
+  def _handle_expiration (self):
+    # Called by a timer so that we can remove old items.
+    empty = []
+    for k,v in self.lost_buffers.iteritems():
+      dpid,ip = k
+
+      for item in list(v):
+        expires_at,buffer_id,in_port = item
+        if expires_at < time.time():
+          # This packet is old.  Tell this switch to drop it.
+          v.remove(item)
+          po = of.ofp_packet_out(buffer_id = buffer_id, in_port = in_port)
+          core.openflow.sendToDPID(dpid, po)
+      if len(v) == 0: empty.append(k)
+
+    # Remove empty buffer bins
+    for k in empty:
+      del self.lost_buffers[k]
+
+  def _send_lost_buffers (self, dpid, ipaddr, macaddr, port):
+    """
+    We may have "lost" buffers -- packets we got but didn't know
+    where to send at the time.  We may know now.  Try and see.
+    """
+    if (dpid,ipaddr) in self.lost_buffers:
+      # Yup!
+      bucket = self.lost_buffers[(dpid,ipaddr)]
+      del self.lost_buffers[(dpid,ipaddr)]
+      log.debug("Sending %i buffered packets to %s from %s"
+                % (len(bucket),ipaddr,dpid_to_str(dpid)))
+      for _,buffer_id,in_port in bucket:
+        po = of.ofp_packet_out(buffer_id=buffer_id,in_port=in_port)
+        po.actions.append(of.ofp_action_dl_addr.set_dst(macaddr))
+        po.actions.append(of.ofp_action_output(port = port))
+        core.openflow.sendToDPID(dpid, po)
+
+  def _handle_GoingUpEvent (self, event):
+    self.listenTo(core.openflow)
+    log.debug("Up...")
 
   def _handle_PacketIn (self, event):
-    """
-    Handle packet in messages from the switch to implement above algorithm.
-    """
-
+    dpid = event.connection.dpid
+    inport = event.port
     packet = event.parsed
+    if not packet.parsed:
+      log.warning("%i %i ignoring unparsed packet", dpid, inport)
+      return
 
-    def flood (message = None):
-      """ Floods the packet """
-      msg = of.ofp_packet_out()
-      if time.time() - self.connection.connect_time >= _flood_delay:
-        # Only flood if we've been connected for a little while...
+    if dpid not in self.arpTable:
+      # New switch -- create an empty table
+      self.arpTable[dpid] = {}
+      for fake in self.fakeways:
+        self.arpTable[dpid][IPAddr(fake)] = Entry(of.OFPP_NONE,
+         dpid_to_mac(dpid))
 
-        if self.hold_down_expired is False:
-          # Oh yes it is!
-          self.hold_down_expired = True
-          log.info("%s: Flood hold-down expired -- flooding",
-              dpid_to_str(event.dpid))
+    if packet.type == ethernet.LLDP_TYPE:
+      # Ignore LLDP packets
+      return
 
-        if message is not None: log.debug(message)
-        #log.debug("%i: flood %s -> %s", event.dpid,packet.src,packet.dst)
-        # OFPP_FLOOD is optional; on some switches you may need to change
-        # this to OFPP_ALL.
-        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+    if isinstance(packet.next, ipv4):
+      log.debug("%i %i IP %s => %s", dpid,inport,
+                packet.next.srcip,packet.next.dstip)
+
+      # Send any waiting packets...
+      self._send_lost_buffers(dpid, packet.next.srcip, packet.src, inport)
+
+      # Learn or update port/MAC info
+      if packet.next.srcip in self.arpTable[dpid]:
+        if self.arpTable[dpid][packet.next.srcip] != (inport, packet.src):
+          log.info("%i %i RE-learned %s", dpid,inport,packet.next.srcip)
       else:
-        pass
-        #log.info("Holding down flood for %s", dpid_to_str(event.dpid))
-      msg.data = event.ofp
-      msg.in_port = event.port
-      self.connection.send(msg)
+        log.debug("%i %i learned %s", dpid,inport,str(packet.next.srcip))
+      self.arpTable[dpid][packet.next.srcip] = Entry(inport, packet.src)
 
-    def drop (duration = None):
-      """
-      Drops this packet and optionally installs a flow to continue
-      dropping similar ones for a while
-      """
-      if duration is not None:
-        if not isinstance(duration, tuple):
-          duration = (duration,duration)
-        msg = of.ofp_flow_mod()
-        msg.match = of.ofp_match.from_packet(packet)
-        msg.idle_timeout = duration[0]
-        msg.hard_timeout = duration[1]
-        msg.buffer_id = event.ofp.buffer_id
-        self.connection.send(msg)
-      elif event.ofp.buffer_id is not None:
-        msg = of.ofp_packet_out()
-        msg.buffer_id = event.ofp.buffer_id
-        msg.in_port = event.port
-        self.connection.send(msg)
+      # Try to forward
+      dstaddr = packet.next.dstip
+      if dstaddr in self.arpTable[dpid]:
+        # We have info about what port to send it out on...
 
-    self.macToPort[packet.src] = event.port # 1
+        prt = self.arpTable[dpid][dstaddr].port
+        mac = self.arpTable[dpid][dstaddr].mac
+        if prt == inport:
+          log.warning("%i %i not sending packet for %s back out of the " +
+                      "input port" % (dpid, inport, str(dstaddr)))
+        else:
+          log.debug("%i %i installing flow for %s => %s out port %i"
+                    % (dpid, inport, packet.next.srcip, dstaddr, prt))
 
-    if not self.transparent: # 2
-      if packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered():
-        drop() # 2a
-        return
+          actions = []
+          actions.append(of.ofp_action_dl_addr.set_dst(mac))
+          actions.append(of.ofp_action_output(port = prt))
+          match = of.ofp_match.from_packet(packet, inport)
+          match.dl_src = None # Wildcard source MAC
 
-    if packet.dst.is_multicast:
-      flood() # 3a
-    else:
-      if packet.dst not in self.macToPort: # 4
-        flood("Port for %s unknown -- flooding" % (packet.dst,)) # 4a
-      else:
-        port = self.macToPort[packet.dst]
-        if port == event.port: # 5
-          # 5a
-          log.warning("Same port for packet from %s -> %s on %s.%s.  Drop."
-              % (packet.src, packet.dst, dpid_to_str(event.dpid), port))
-          drop(10)
+          msg = of.ofp_flow_mod(command=of.OFPFC_ADD,
+                                idle_timeout=FLOW_IDLE_TIMEOUT,
+                                hard_timeout=of.OFP_FLOW_PERMANENT,
+                                buffer_id=event.ofp.buffer_id,
+                                actions=actions,
+                                match=of.ofp_match.from_packet(packet,
+                                                               inport))
+          event.connection.send(msg.pack())
+      elif self.arp_for_unknowns:
+        # We don't know this destination.
+        # First, we track this buffer so that we can try to resend it later
+        # if we learn the destination, second we ARP for the destination,
+        # which should ultimately result in it responding and us learning
+        # where it is
+
+        # Add to tracked buffers
+        if (dpid,dstaddr) not in self.lost_buffers:
+          self.lost_buffers[(dpid,dstaddr)] = []
+        bucket = self.lost_buffers[(dpid,dstaddr)]
+        entry = (time.time() + MAX_BUFFER_TIME,event.ofp.buffer_id,inport)
+        bucket.append(entry)
+        while len(bucket) > MAX_BUFFERED_PER_IP: del bucket[0]
+
+        # Expire things from our outstanding ARP list...
+        self.outstanding_arps = {k:v for k,v in
+         self.outstanding_arps.iteritems() if v > time.time()}
+
+        # Check if we've already ARPed recently
+        if (dpid,dstaddr) in self.outstanding_arps:
+          # Oop, we've already done this one recently.
           return
-        # 6
-        log.debug("installing flow for %s.%i -> %s.%i" %
-                  (packet.src, event.port, packet.dst, port))
-        msg = of.ofp_flow_mod()
-        msg.match = of.ofp_match.from_packet(packet, event.port)
-        msg.idle_timeout = 10
-        msg.hard_timeout = 30
-        msg.actions.append(of.ofp_action_output(port = port))
-        msg.data = event.ofp # 6a
-        self.connection.send(msg)
+
+        # And ARP...
+        self.outstanding_arps[(dpid,dstaddr)] = time.time() + 4
+
+        r = arp()
+        r.hwtype = r.HW_TYPE_ETHERNET
+        r.prototype = r.PROTO_TYPE_IP
+        r.hwlen = 6
+        r.protolen = r.protolen
+        r.opcode = r.REQUEST
+        r.hwdst = ETHER_BROADCAST
+        r.protodst = dstaddr
+        r.hwsrc = packet.src
+        r.protosrc = packet.next.srcip
+        e = ethernet(type=ethernet.ARP_TYPE, src=packet.src,
+                     dst=ETHER_BROADCAST)
+        e.set_payload(r)
+        log.debug("%i %i ARPing for %s on behalf of %s" % (dpid, inport,
+         str(r.protodst), str(r.protosrc)))
+        msg = of.ofp_packet_out()
+        msg.data = e.pack()
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        msg.in_port = inport
+        event.connection.send(msg)
+
+    elif isinstance(packet.next, arp):
+      a = packet.next
+      log.debug("%i %i ARP %s %s => %s", dpid, inport,
+       {arp.REQUEST:"request",arp.REPLY:"reply"}.get(a.opcode,
+       'op:%i' % (a.opcode,)), str(a.protosrc), str(a.protodst))
+
+      if a.prototype == arp.PROTO_TYPE_IP:
+        if a.hwtype == arp.HW_TYPE_ETHERNET:
+          if a.protosrc != 0:
+
+            # Learn or update port/MAC info
+            if a.protosrc in self.arpTable[dpid]:
+              if self.arpTable[dpid][a.protosrc] != (inport, packet.src):
+                log.info("%i %i RE-learned %s", dpid,inport,str(a.protosrc))
+            else:
+              log.debug("%i %i learned %s", dpid,inport,str(a.protosrc))
+            self.arpTable[dpid][a.protosrc] = Entry(inport, packet.src)
+
+            # Send any waiting packets...
+            self._send_lost_buffers(dpid, a.protosrc, packet.src, inport)
+
+            if a.opcode == arp.REQUEST:
+              # Maybe we can answer
+
+              if a.protodst in self.arpTable[dpid]:
+                # We have an answer...
+
+                if not self.arpTable[dpid][a.protodst].isExpired():
+                  # .. and it's relatively current, so we'll reply ourselves
+
+                  r = arp()
+                  r.hwtype = a.hwtype
+                  r.prototype = a.prototype
+                  r.hwlen = a.hwlen
+                  r.protolen = a.protolen
+                  r.opcode = arp.REPLY
+                  r.hwdst = a.hwsrc
+                  r.protodst = a.protosrc
+                  r.protosrc = a.protodst
+                  r.hwsrc = self.arpTable[dpid][a.protodst].mac
+                  e = ethernet(type=packet.type, src=dpid_to_mac(dpid),
+                               dst=a.hwsrc)
+                  e.set_payload(r)
+                  log.debug("%i %i answering ARP for %s" % (dpid, inport,
+                   str(r.protosrc)))
+                  msg = of.ofp_packet_out()
+                  msg.data = e.pack()
+                  msg.actions.append(of.ofp_action_output(port =
+                                                          of.OFPP_IN_PORT))
+                  msg.in_port = inport
+                  event.connection.send(msg)
+                  return
+
+      # Didn't know how to answer or otherwise handle this ARP, so just flood it
+      log.debug("%i %i flooding ARP %s %s => %s" % (dpid, inport,
+       {arp.REQUEST:"request",arp.REPLY:"reply"}.get(a.opcode,
+       'op:%i' % (a.opcode,)), str(a.protosrc), str(a.protodst)))
+
+      msg = of.ofp_packet_out(in_port = inport, data = event.ofp,
+          action = of.ofp_action_output(port = of.OFPP_FLOOD))
+      event.connection.send(msg)
 
 
-class lab3_pox (object):
-  """
-  Waits for OpenFlow switches to connect and makes them learning switches.
-  """
-  def __init__ (self, transparent):
-    core.openflow.addListeners(self)
-    self.transparent = transparent
+def launch (fakeways="", arp_for_unknowns=None):
+  fakeways = fakeways.replace(","," ").split()
+  fakeways = [IPAddr(x) for x in fakeways]
+  if arp_for_unknowns is None:
+    arp_for_unknowns = len(fakeways) > 0
+  else:
+    arp_for_unknowns = str_to_bool(arp_for_unknowns)
+  core.registerNew(lab3_switch, fakeways, arp_for_unknowns)
 
-  def _handle_ConnectionUp (self, event):
-    log.debug("Connection %s" % (event.connection,))
-    LearningSwitch(event.connection, self.transparent)
-
-
-def launch (transparent=False, hold_down=_flood_delay):
-  """
-  Starts the simple router for lab3.
-  """
-  try:
-    global _flood_delay
-    _flood_delay = int(str(hold_down), 10)
-    assert _flood_delay >= 0
-  except:
-    raise RuntimeError("Expected hold-down to be a number")
-
-  core.registerNew(lab3_pox, str_to_bool(transparent))
