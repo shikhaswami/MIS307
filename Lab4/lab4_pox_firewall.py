a0d1fef29e0518ae5a007f42aecf05becb969c66
Wei Chen <weichen@email.arizona.edu>
2016-08-06 17:27:20 -0700

    simplified the lab setup; shell script written

--- Lab4/lab4_pox_firewall.py
+++ Lab4/lab4_pox_firewall.py
@@ -1,38 +1,4 @@
 #!/usr/bin/python
-# Copyright 2012 William Yu
-# wyu@ateneo.edu
-#
-# This file is part of POX.
-#
-# POX is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# POX is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with POX. If not, see <http://www.gnu.org/licenses/>.
-#
-# NOISY FIREWALL. This is a simple firewall implementation for demonstration
-# purposes. This firewall is bad because only flows for valid packets are
-# installed. Non-matches always trigger a PacketIn().
-#
-# This is a demonstration file aims to build a firewall. In this demo, 
-# firewall rules are applied to specific ports in the switch using the
-# following commands:
-#   AddRule (event, dl_type=0x800, nw_proto=1, port=0, src_port=of.OFPP_ALL)
-#   DeleteRule (event, dl_type=0x800, nw_proto=1, port=0, src_port=of.OFPP_ALL):
-#   ShowRule ()
-#
-# Mininet Command Line: sudo mn --topo single,3 --mac --switch ovsk --controller remote
-# Command Line: ./pox.py py log.level --DEBUG samples.of_firewall
-#
-# THIS VERSION SUPPORT resend() functionality in the betta branch POX.
-#
 
 # These next two imports are common POX convention
 from pox.core import core
